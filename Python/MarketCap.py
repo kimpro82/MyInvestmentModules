ee80be31a9203ccb4726a98950e72e558c52be9d
Kangrok Kim <kimpro@uebermensch.tech>
2021-12-23 02:39:16 +0900

    Update MarketCap.py

--- Python/MarketCap.py
+++ Python/MarketCap.py
@@ -55,9 +55,9 @@ for item in items :
         list = [1, 2, 3, 15]
     else :
         list = [1, 2, 3, 11]
-    length = [4, 20, 10, 10]
+    length = [4, 20, 9, 9]
 
-    for i in range(len(list)) :                                 # All the ditances between neighboring columns are 20
+    for i in range(len(list)) :
         spaces = length[i]
         for char in sinfo[list[i]] :
             if char >= '가' :                                   # Count 2 spaces when the letter is Korean
@@ -66,7 +66,7 @@ for item in items :
                 spaces -= 1
         sinfo[list[i]] += spaces * ' '
 
-        print(sinfo[list[i]], end='')
+        print(sinfo[list[i]], end=' ')
 
     print()
 
@@ -77,4 +77,63 @@ for item in items :
 4   삼성바이오로직스    901,000   596,147
 5   삼성전자우          71,700    590,010
 ……
+'''
+
+
+# 2.2 Read plural pages' data
+
+pages = 2                                                       # input the last page's number
+
+for page in range(1, pages + 1) :
+
+    # Read data in each page
+    params['page'] = page                                       # change the page number in the url
+    response = requests.get(url, params=params)
+    soup = BeautifulSoup(response.content, "html.parser")
+    section = soup.find('tbody')
+    items = section.find_all('tr', onmouseover="mouseOver(this)")
+
+    # Get specific elements
+    for item in items :
+        basic_info = item.get_text()
+        sinfo = basic_info.split("\n")
+
+        # Set the required elements' location and their maximum size
+        if sinfo[5] != '0' :                                    # Data locations are moved when the price change is 0
+            list = [1, 2, 3, 15]
+        else :
+            list = [1, 2, 3, 11]
+        length = [4, 22, 9, 9]                                  # len("한국타이어앤테크놀로지") = 22
+
+        # Print data
+        for i in range(len(list)) :
+
+            spaces = length[i]
+
+            # Find the exact length of the text
+            for char in sinfo[list[i]] :
+                if char >= '가' :                               # Count 2 spaces when the letter is Korean
+                    spaces -= 2
+                else :
+                    spaces -= 1
+
+            # Determine left or right alignment
+            if sinfo[list[i]][0] < 'A' :
+                sinfo[list[i]] = spaces * ' ' + sinfo[list[i]]
+            else :
+                sinfo[list[i]] += spaces * ' '
+
+            # Print
+            print(sinfo[list[i]], end=' ')
+
+        print()
+
+'''
+   1 삼성전자                  79,400 4,740,007
+   2 SK하이닉스               127,000   924,563
+   3 NAVER                    378,500   621,737
+……
+  73 한국타이어앤테크놀로지    39,850    49,364
+……
+ 100 한솔케미칼               293,500    33,269
 '''
