88ba7449b14fef490843759a1356db31b6c16e1c
Kangrok Kim <kimpro@uebermensch.tech>
2023-07-26 08:33:25 +0900

    Update request_t1716.py

--- eBest_OpenAPI/request_t1716.py
+++ eBest_OpenAPI/request_t1716.py
@@ -2,7 +2,21 @@
 eBest Open API / 외인기관종목별동향 (t1716) 실행
 2023.07.25
 
-History :
+이 코드는 eBest Open API에서 t1716 TR을 호출하여 주어진 종목 코드와 조회 기간에 따른 외인과 기관의 순매매 정보를 조회하고,
+이를 하나의 DataFrame으로 병합한 뒤 하나의 CSV 파일로 저장하는 작업을 수행합니다.
+
+Parameters  :
+    TR_NAME (str)       : TR을 호출하는 함수의 이름으로 사용될 문자열입니다.
+    shcodes (list)      : 조회할 종목 코드들이 담긴 리스트입니다.
+    todts (list)        : 조회를 종료할 날짜들이 담긴 리스트입니다.
+    YEARS (int)         : 조회 기간의 연도 수를 나타내는 정수입니다.
+    PERIOD (int)        : 조회 기간을 나타내는 정수로, 최대 366일까지 조회가 가능합니다.
+    unique_keys (list)  : 중복된 열을 제거하기 위해 사용될 DataFrame의 열 이름들이 담긴 리스트입니다.
+
+Returns     :
+    None
+
+History     :
     1   2023.07.21  최초 작성
     2   2023.07.25  request_tr()의 리턴값을 병합하여 하나의 CSV 파일로 출력
 """
